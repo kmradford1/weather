a06f2ca7cb52cbe44e35a442b98f3bc2b5770676
Kimberly Radford <kimberly.radford@eagles.ewu.edu>
2018-10-30 11:40:16 -0700

    got weather
    bork bork

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 console.log( $( ) );
 
-console.log( 'ok' );
+$.simpleWeather({
+    location: 99004,
+    success: function(weather) {
+        console.log(weather.image);
+        console.log(weather.updated);
+        console.log(weather.temp);
+        console.log(weather.city);
+
+        $('.image img').attr('src',weather.image);
+        $('.city').text(weather.updated);
+        $('.temp').text(weather.temp);
+        $('.time').text(weather.city);
+    }, 
+    error: function(error) {
+      console.log('Fucking look outside!');
+    }
+ });
